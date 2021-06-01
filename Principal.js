79b65cddd3381c03726170e092f515cbdda0b9e7
MarcelaRivera2000 <marci.eunicer@gmail.com>
2021-05-31 20:43:27 -0600

    Primer dia

---
+++ Principal.js
@@ -0,0 +1,9 @@
+class Apunte {
+    constructor(descripcion, fecha,likes,dislikes) {
+      this.descripcion = descripcion;
+      this.fecha = fecha;
+      this.likes = likes;
+      this.dislikes = dislikes;
+    }
+    
+  }
